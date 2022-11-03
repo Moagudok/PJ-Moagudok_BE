f096daaa2cc00ec022c8697b9aeb3861dddd55be
SeonminKim1 <yubi5050@naver.com>
2022-11-03 20:43:43 +0900

    Add django-dotenv

--- AuthService/AuthService/settings.py
+++ AuthService/AuthService/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h5%-%rv_n*jx%k6j-l&-luqhdx_9#k2kpi3y_!lpxjbo$!n+h$'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,10 +75,24 @@ WSGI_APPLICATION = 'AuthService.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+''' # sqlite
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR.parent / 'db.sqlite3',
+    }
+}
+'''
+
+# PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get("DB_ENGINE"),
+        'NAME': os.environ.get("DB_NAME"), # Schema Name
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"), # PASSWORD NAME
+        'HOST':os.environ.get("DB_HOST"),
+        'PORT':os.environ.get("DB_PORT"),
     }
 }
 
