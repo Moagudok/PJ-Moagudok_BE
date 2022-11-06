debd0048645bbd2cb69537bf5d8b2022a75739e5
SeonminKim1 <yubi5050@naver.com>
2022-11-07 02:20:16 +0900

    Add Init Base Setting

---
+++ _utils/set_basedb.sql
@@ -0,0 +1,11 @@
+INSERT INTO public."SignupMethod"(id, method)
+	VALUES 
+	(1, 'basic'),
+	(2, 'google');
+
+INSERT INTO public."Category"(id, name, image)
+	VALUES 
+	(1, 'coffee', 'https://www.notion.so/b1181e58e1c947739b2022dcfed9e6c8#44662a62706046858c1f3e3cb504aaef'),
+	(2, 'meat', 'https://www.notion.so/b1181e58e1c947739b2022dcfed9e6c8#d9cfdc4c419f4fb18ec880e162ff7a6a'),
+	(3, 'crawling', 'https://www.notion.so/b1181e58e1c947739b2022dcfed9e6c8#ef93029e30e440f1974a769890b4a1a3'),
+	(4, 'munchies', 'https://www.notion.so/b1181e58e1c947739b2022dcfed9e6c8#e7662ac3dcd34d2a9d8927d360c2bee4');
