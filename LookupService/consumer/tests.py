380f3b16561390dbc948e76aeb01f120596a0fc6
SeonminKim1 <yubi5050@naver.com>
2022-11-10 16:26:07 +0900

    Add Testcode in CategoryList Pagination #8

--- LookupService/consumer/tests.py
+++ LookupService/consumer/tests.py
@@ -1,15 +1,15 @@
 import pytest
-from sharedb.models import Category, ProductImages, Product, User, SignupMethod, PaymentTerm
+from sharedb.models import Category
 
 '''
 카테고리 리스트 조회 화면 TESTCODE
+결함 TEST 위주
+- Category 값 중 누락된건 없는지
+- 모든 Rows에 name과 image값이 다 있는지
 '''
 @pytest.mark.skip()
 # @pytest.mark.django_db
 class TestCategory():
-    ## 결함 TEST 
-    # - Category 값 중 누락된건 없는지
-    # - 모든 Rows에 name과 image값이 다 있는지
     def test_CategoryRowsValidation(self, CreateCategories):
         queryset = list(Category.objects.all())
         assert len(queryset)==12, "Num of Category Rows is Failed"
@@ -26,6 +26,9 @@ class TestCategory():
 
 '''
 상품 리스트 조회 (by 카테고리) 화면 TESTCODE
+- 페이지 별 항목(row) 갯수가 맞는지
+- 첫 페이지 항목 속성 검사, 상태코드 검사
+- 모든 페이지가 정상적으로 다 반환되는지 (속성, 상태코드)
 '''
 # @pytest.mark.skip()
 @pytest.mark.django_db
@@ -36,14 +39,22 @@ class TestProductList():
     def test_ProductListbyCategory(self, CreateCategories, \
         CreateSignupMethod, CreateUser, CreatePaymentTerm, \
         CreateProducts, CreateProductImages, client):
-        resp = client.get("/consumer/product/list?category=1")
-        assert len(resp.data) >= 0
-        assert resp.status_code == 200
-        
-        resp = client.get("/consumer/product/list?category=500")
-        assert resp.data['message'] == 'There are no products in this category'
+        PAGE_SIZE = 2
+        resp = client.get("/consumer/product/list?category=1&page=1")
+        assert len(resp.data['results']) == PAGE_SIZE
+        assert resp.data['previous'] == None
         assert resp.status_code == 200
 
-        resp = client.get("/consumer/product/list?category=abc")
-        assert resp.data['message'] == 'Params is invalid'
-        assert resp.status_code == 400
+        # 마지막 page 번호 구하기
+        PAGE_COUNT = resp.data['count'] // PAGE_SIZE
+        if resp.data['count'] % PAGE_SIZE != 0:
+            PAGE_COUNT+=1
+
+        # 통과 갯수 - 모든 Request 가 잘 들어왔는지
+        pass_count = 0
+        for i in range(1, PAGE_COUNT+1):
+            resp = client.get("/consumer/product/list?category=1&page="+str(i)) 
+            assert len(resp.data['results']) == PAGE_SIZE
+            assert resp.status_code == 200
+            pass_count+=1
+        assert pass_count == PAGE_COUNT
