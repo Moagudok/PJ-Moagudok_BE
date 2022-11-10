ec5bbf2d5a7e91f5b5e78fb5e4086b0ae3b19932
SeonminKim1 <yubi5050@naver.com>
2022-11-10 18:53:18 +0900

    Add TextResultList Pagination by Viewset, DRF PageNumberPagination #7

--- LookupService/consumer/views.py
+++ LookupService/consumer/views.py
@@ -1,8 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import viewsets, status
-from rest_framework.pagination import PageNumberPagination # 페이지 기반 파지네이션 import
-
+from rest_framework.pagination import PageNumberPagination
+from django.db.models import Q
 from sharedb.models import Category, Product
 from .serializers import ProductCategoryListSerializer, ProductListSerializer
 
@@ -13,18 +13,26 @@ class ProductCategoryListView(APIView):
         CategorySerializer_data = ProductCategoryListSerializer(category_data, many=True).data
         return Response(CategorySerializer_data, status.HTTP_200_OK)
 
-
-class CategoryListPaginationClass(PageNumberPagination): # 
+class ProductListPaginationClass(PageNumberPagination): # 
     page_size = 2 # settings.py의 Default 값 변경
 
-# url : /consumer/product/list?category=1&page=1
-class CategoryListPaginationViewSet(viewsets.ModelViewSet):
+# url : /consumer/product/list?category=1&search&page=1
+class ProductListPaginationViewSet(viewsets.ModelViewSet):
     serializer_class = ProductListSerializer
-    pagination_class = CategoryListPaginationClass
+    pagination_class = ProductListPaginationClass
     queryset = Product.objects.all()
     def get_queryset(self):
+        condition = Q()
         category_id = self.request.query_params['category']
-        query_set = self.queryset.filter(category_id=category_id)
+        search_text = self.request.query_params['search']
+        if category_id:
+            condition.add(Q(category_id = category_id), condition.AND)
+        if search_text:
+            mini_q = Q()
+            mini_q.add(Q(product_name__icontains = search_text), condition.OR) # product_name - 대소문자 구분 X 검색
+            mini_q.add(Q(product_group_name__icontains = search_text), condition.OR) # product_group_name - 대소문자 구분 X 검색
+            condition.add(mini_q, condition.AND)
+        query_set = self.queryset.filter(condition)
         return query_set
 
 
