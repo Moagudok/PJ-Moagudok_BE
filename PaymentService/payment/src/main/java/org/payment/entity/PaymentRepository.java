39967e2d338a9ba74d7367bea2591dc91593586f
Aeius <psb6604@gmail.com>
2022-11-19 09:04:05 +0900

    update jarfile

--- PaymentService/payment/src/main/java/org/payment/entity/PaymentRepository.java
+++ PaymentService/payment/src/main/java/org/payment/entity/PaymentRepository.java
@@ -6,11 +6,8 @@
 package org.payment.entity;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
