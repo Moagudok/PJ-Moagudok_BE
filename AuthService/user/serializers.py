b5583e25725eee378a5af798ad97aeebb83ea619
Aeius <psb6604@gmail.com>
2022-11-21 14:27:34 +0900

    Add user join validation

--- AuthService/user/serializers.py
+++ AuthService/user/serializers.py
@@ -19,7 +19,16 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
-        
+    
+    def validate_name(self, data):
+        if len(data) < 2:
+            raise serializers.ValidationError("이름은 2자 이상이어야 합니다.")
+    def validate_password(self, data):
+        if len(data) < 8:
+            raise serializers.ValidationError("비밀번호는 8자 이상이어야 합니다.")
+    def validate_address(self, data):
+        if len(data) < 5:
+            raise serializers.ValidationError("주소는 5자 이상이어야 합니다.")
 
 class LoginUserSerializer(serializers.Serializer):
     email = serializers.EmailField()
