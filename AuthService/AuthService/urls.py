1a71bb61863939eb577a24cdefa19ba67a17e534
Aeius <psb6604@gmail.com>
2022-11-28 02:08:18 +0900

    Fix refresh token - add email claim

--- AuthService/AuthService/urls.py
+++ AuthService/AuthService/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('user', UserAPIView.as_view()),
     path('user/login', LoginUserAPIView.as_view()),
     path('user/join', JoinUserAPIView.as_view(), name='userjoin'),
-    path('user/token', TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path('user/token/refresh', TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- AuthService/user/urls.py
+++ AuthService/user/urls.py
@@ -5,7 +5,5 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView)
 
 urlpatterns = [
-    path('sign_up/', views.UserView.as_view(), name="user"),
-    path('sign_in/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
 ]
