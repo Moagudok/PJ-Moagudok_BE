8d1eff84bcd7fe9b1deaebc500759b73f8eb062a
Aeius <psb6604@gmail.com>
2022-11-28 00:44:55 +0900

    hotfix

--- PaymentService/payment/src/main/java/org/payment/uitl/Constants.java
+++ PaymentService/payment/src/main/java/org/payment/uitl/Constants.java
@@ -3,5 +3,5 @@ package org.payment.uitl;
 public final class Constants {
     public static final String AWS_IP = "54.180.145.47";
     public static final String PORT_LOOKUP = ":10001";
-    public static final String PORT_MAIL = ":8005";
+    public static final String PORT_MAIL = ":8004";
 }
